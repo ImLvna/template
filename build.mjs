7f3f62f1cc7f0ebaf38f50d367c847ea3e132db1
Luna <imlvnaa@gmail.com>
2024-02-15 13:45:40 -0500

    no more esbuild

--- build.mjs
+++
@@ -1,9 +0,0 @@
-import * as esbuild from "esbuild";
-
-esbuild.build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  outfile: "dist/index.js",
-  platform: "node",
-  target: ["node21"],
-});
