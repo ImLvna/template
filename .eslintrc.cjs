00800a4ffdc98a81b1c1e5d489e010018095329f
Luna <imlvnaa@gmail.com>
2024-04-30 11:35:46 -0400

    eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'prettier'],
+	env: {
+		node: true
+	}
+};
